6b060f1046edbf81cc7051db6912b48e57e10941
Pavel <grigorov_ps@mail.ru>
2023-10-28 15:42:46 +0300

    Создали проект Task64 (Которая выдает все натуральные числа в промежутке от 1 до N с помощю хвостовой Рекурсии

---
+++ Task64/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64
+// Задайте значение N 
+// Напишите программу которая выдает все натуральные числа
+// в промежутке от 1 до N
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void NaturalNumbers(int num)
+{
+    if(num == 0) return; 
+    Console.Write($"{num} ");
+    NaturalNumbers(num - 1);
+}
+
+
+Console.WriteLine("Введите натуральное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 0) 
+{
+    Console.Write($"{number} -> ");
+    NaturalNumbers(number);
+}
+else Console.WriteLine("Некорректный ввод");
+return;
